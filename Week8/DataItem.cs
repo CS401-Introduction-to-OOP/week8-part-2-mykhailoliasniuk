01b8c652f85abfa058d2ae95f303aab22493bf73
Misha <mykhailo.liasniuk.25@kse.org.ua>
2026-03-28 17:31:51 +0200

    3) 1 stage

---
+++ Week8/DataItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Week8;
+
+public class DataItem
+{
+    public string Id { get; set; }
+    public byte[] Pyload { get; set; }
+
+    public int GetSize()
+    {
+        return Pyload.Length;
+    }
+}
