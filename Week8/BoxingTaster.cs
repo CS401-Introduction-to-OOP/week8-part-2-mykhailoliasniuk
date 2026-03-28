fd61213a5a0e1916c80f4155a98e55a82109f3b8
Misha <mykhailo.liasniuk.25@kse.org.ua>
2026-03-28 17:26:24 +0200

    2 stage

---
+++ Week8/BoxingTaster.cs
@@ -0,0 +1,12 @@
+﻿namespace Week8;
+
+public class BoxingTaster
+{
+    public static void Test()
+    {
+        int a = 10;
+        object b = a;//boxing, creates reference to value a
+        int c = (int)b;//unboxing, gets value
+        Console.WriteLine($"a - {a}, b - {b}, c - {c}");
+    }
+}
