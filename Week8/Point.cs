33e916826a88419910a418f5cace3f82cee5b68f
Misha <mykhailo.liasniuk.25@kse.org.ua>
2026-03-28 17:14:45 +0200

    2) stage 1

---
+++ Week8/Point.cs
@@ -0,0 +1,12 @@
+﻿namespace Week8;
+
+public struct Point
+{
+    public int X;
+    public int Y;
+
+    public void Print()
+    {
+        Console.WriteLine($"Coordinates: ({X}, {Y})");
+    }
+}
