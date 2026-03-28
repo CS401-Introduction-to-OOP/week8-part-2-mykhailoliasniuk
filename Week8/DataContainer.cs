339c82d4a769587ba1e8060018e1a7e7368d6115
Misha <mykhailo.liasniuk.25@kse.org.ua>
2026-03-28 17:35:47 +0200

    2 stage

---
+++ Week8/DataContainer.cs
@@ -0,0 +1,22 @@
+﻿namespace Week8;
+
+public class DataContainer<T> where T : DataItem
+{
+    private List<DataItem> _items = new List<DataItem>();
+
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+
+    public int GetTotalSize()
+    {
+        int res = 0;
+        foreach (var item in _items)
+        {
+            res += item.GetSize();
+        }
+
+        return res;
+    }
+}
